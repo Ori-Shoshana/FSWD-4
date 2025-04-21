250bd0e6cf36f4f15d38187a2693d367feae67f4
Ori-Shoshana <116821177+Ori-Shoshana@users.noreply.github.com>
2025-04-21 21:09:08 +0300

    Delete src directory

--- src/components/TextDisplay.jsx
+++
@@ -1,7 +0,0 @@
-export default function TextDisplay({ text, style }) {
-  return (
-    <div className="text-display" style={style}>
-      {text || <span className="placeholder">Start typing below...</span>}
-    </div>
-  );
-}
